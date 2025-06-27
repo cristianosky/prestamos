a43c0d5733b45e41553ab4e7a796b9d8e2a11a1b
Cristian Villalba <cristianvillalba1999@gmail.com>
2025-06-27 10:09:46 -0500

    Mejora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "prestame-test", appId: "1:680541066966:web:d9de5ed2a352320bf8db77", storageBucket: "prestame-test.firebasestorage.app", apiKey: "AIzaSyBbBSzuIm7te3yaWznD5s9CbD5IqX-NdN8", authDomain: "prestame-test.firebaseapp.com", messagingSenderId: "680541066966", measurementId: "G-GKMLYLWDFK" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
